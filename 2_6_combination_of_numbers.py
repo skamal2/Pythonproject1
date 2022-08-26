56d3b0a793a3a9e6df8b20a8be94091ae89f71c6
skamal2 <kkamal_sigdel2000@hotmail.com>
2022-08-26 23:33:49 +0300

    Pythonexcercises

--- 2_6_combination_of_numbers.py
+++ 2_6_combination_of_numbers.py
@@ -1,12 +1,12 @@
- import Math
- import
- r1 = random.radiant(0,9)
- r2 = random.randit(0,9)
- r3 = random.radiant(0,9)
+import random
+import math
+r1 = random.randint(0,9)
+r2 = random.randint(0,9)
+r3 = random.randint(0,9)
 
- print(f"Random code {r1,r2,r3}")
- r1 = random.radiant(1, 6)
- r2 = random.randit(1, 6)
- r3 = random.radiant(1, 6)
- r4 = random.radiant(1, 6)
- print(f"Random code {r1,r2,r3,r4}")
+print(f"Random code {r1,r2,r3}")
+r1 = random.randint(1, 6)
+r2 = random.randint(1, 6)
+r3 = random.randint(1, 6)
+r4 = random.randint(1, 6)
+print(f"Random code {r1,r2,r3,r4}")
