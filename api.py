cf7b5bafb6688f28e7d2133d83a7feb279251bef
skamal2 <kkamal_sigdel2000@hotmail.com>
2022-10-10 15:28:16 +0300

    Merge remote-tracking branch 'github/master'

--- api.py
+++ api.py
@@ -1,30 +1,14 @@
-
 import requests
-player_firstname = input("Enter first name: ")
-player_lasttname = input("Enter last name: ")
-last_confirmation =input("Press Enter to start the game. ")
-if last_confirmation == "":
-    print("Welcome To The Game!")
-#The given weather conditions are:
-print("Your goal is to reach all of the airports having given weather conditions with given energy.")
-#Congratulations! One goal reached.
-#Unfortunately you did not reach any goals!
-#current weather
-#current location,
-# distance between origin and destination,
-# consumed energy,
-# available energy, achieved goals,
-# remaining goals, achieved points and
-# required points to get extra energy
-#
 
 base_url = "https://api.openweathermap.org/data/2.5/weather?"
 api_key = "f1c5c2efcc1463620cfe351cb7b40f30"
-city = input("Enter name or the ICAO code of your destination: ")
+city = input("Enter name or the ICAO code of your destination: ").upper()
 def kelvin_fahrenheit_to_celcius(kelvin):
     celcius = kelvin - 273.15
     fahrenheit = celcius*(9/5) + 32
     return celcius, fahrenheit
+
+
 url = base_url + "appid=" + api_key + "&q=" + city
 response = requests.get(url).json()
 temp_kelvin = response["main"]["temp"]
@@ -35,11 +19,8 @@ feels_like_celcius, feels_like_farenheit = kelvin_fahrenheit_to_celcius(feels_li
 description = response["weather"][0]["description"]
 humidity = response["main"]["humidity"]
 
-print(response)
 
 print(f"Temperature in {city}: {temp_celcius:.0f}°C")
 print(f"Temperature in {city} feels like: {feels_like_celcius:.0f}°C")
 
 print(f"General Weather in {city}: {description}")
-
-
